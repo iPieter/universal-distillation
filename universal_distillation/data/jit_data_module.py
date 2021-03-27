283c821b4f480e7c569097aef39c80dc92efc50f
Pieter Delobelle <pieter.delobelle@kuleuven.be>
2021-03-27 11:50:22 +0100

    Fixed #3

---
+++ universal_distillation/data/jit_data_module.py
@@ -0,0 +1,24 @@
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
+
+from .jit_dataloader import JITTokenizedDataset
+
+
+class JITDataModule(LightningDataModule):
+    def __init__(self, file_path, tokenizer):
+        super().__init__()
+        self.file_path = file_path
+        self.tokenizer = tokenizer
+
+    def train_dataloader(self):
+        train_split = JITTokenizedDataset(
+            file_path=self.file_path,
+            tokenizer=self.tokenizer,
+        )
+        return DataLoader(
+            train_split,
+            batch_size=6,
+            collate_fn=train_split.batch_sequences,
+            pin_memory=True,
+            #num_workers=40
+        )
